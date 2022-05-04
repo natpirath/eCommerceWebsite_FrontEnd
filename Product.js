f3bda31a7299049a863e6b5c34d95f9e1a8f8fdf
Natalia Pirath <pira0014@algonquinlive.com>
2022-05-04 18:19:35 -0400

    init

---
+++ Product.js
@@ -0,0 +1,19 @@
+class Product {
+    contructor(
+        name,
+        type,
+        brand,
+        size,
+        price,
+        image,
+    ) {
+        this.name = name;
+        this.type = type;
+        this.brand = brand;
+        this.size = size;
+        this.price = price;
+        this.image = image;
+    }
+}
+
+export default Product;
